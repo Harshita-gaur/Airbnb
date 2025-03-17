d317835d8b83aecb2ce390906d6d537fc3748655
Harshita Gaur <103564814+Harshita-gaur@users.noreply.github.com>
2025-03-17 20:12:48 +0530

    Update settings.py

--- airbnb_backend/airbnb_backend/settings.py
+++ airbnb_backend/airbnb_backend/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'airbnb_db',
         'USER': 'root',
-        'PASSWORD': 'MySQL123@',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
